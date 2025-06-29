99d5725afd3bf3341f50a4e57b71295d5d5ecc3f
Your Name <you@example.com>
2025-06-29 18:23:11 +0000

    my commit

---
+++ my_project/modules/main.tf
@@ -0,0 +1,4 @@
+module "iam_iam-user" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "5.28.0"
+}
---
+++ my_project/modules/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.34.0"
+    }
+   time = {
+      source  = "hashicorp/time"
+      version = "0.7.2"  # Make sure to use the version that match latest version
+    }
+
+  }
+}
+
+
+
